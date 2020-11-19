1208b023e134a5c054511aa01b4872ac308a3347
wata14341 <akki.ni.ni.32@gmail.com>
2020-11-19 21:21:36 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  post 'users/create' => 'users#create'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  get 'posts/:id' => 'posts#show'
+  post 'posts/create' => 'posts#create'
+  get 'posts/:id/edit' => 'posts#edit'
+  post 'posts/:id/update' => 'posts#update'
+  post 'posts/:id/destroy' => 'posts#destroy'
+
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
